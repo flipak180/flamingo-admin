0abd018aa4c797d041a8970bb8d556503ffe6268
skavorodka <flipak180@mail.ru>
2024-10-17 23:41:13 +0300

    +++

--- api/controllers/CategoryController.php
+++ api/controllers/CategoryController.php
@@ -21,13 +21,26 @@ class CategoryController extends BaseApiController
         /** @var Category[] $categories */
         $categories = Category::find()->where(['parent_id' => $parent_id])->orderBy('position ASC')->all();
         foreach ($categories as $category) {
-            $image = EasyThumbnailImage::thumbnailFileUrl(Yii::getAlias('@frontend_web').$category->image, 736, 600, EasyThumbnailImage::THUMBNAIL_OUTBOUND, 100);
-            $result[] = [
+            /** @var Category[] $subcategories */
+            $subcategories = Category::find()->where(['parent_id' => $category->category_id])->orderBy('position ASC')->all();
+            $result[$category->position] = [
                 'id' => $category->category_id,
                 'title' => $category->title,
-                'image' => $image,
+                'subcategories' => [],
             ];
+            foreach ($subcategories as $subcategory) {
+                $image = $subcategory->image
+                    ? EasyThumbnailImage::thumbnailFileUrl(Yii::getAlias('@frontend_web').$subcategory->image, 736, 600, EasyThumbnailImage::THUMBNAIL_OUTBOUND, 100)
+                    : null;
+                $result[$category->position]['subcategories'][] = [
+                    'id' => $subcategory->category_id,
+                    'title' => $subcategory->title,
+                    'image' => $image,
+                ];
+            }
         }
+
+        //ksort($result);
 
         return $result;
     }
