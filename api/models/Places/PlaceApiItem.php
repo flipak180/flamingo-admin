bd8ce04a6bb440f924523976ee1f78adbd58ed90
skavorodka <flipak180@mail.ru>
2025-01-05 02:32:08 +0300

    +++

--- api/models/Places/PlaceApiItem.php
+++ api/models/Places/PlaceApiItem.php
@@ -22,10 +22,12 @@ class PlaceApiItem implements ApiItem
         $images = [];
         $bigImages = [];
         $smallImages = [];
+        $portraitImages = [];
         foreach ($model->images as $image) {
             $images[] = EasyThumbnailImage::thumbnailFileUrl(Yii::getAlias('@frontend_web').$image->path, 344, 344, EasyThumbnailImage::THUMBNAIL_OUTBOUND, 100);
             $bigImages[] = EasyThumbnailImage::thumbnailFileUrl(Yii::getAlias('@frontend_web').$image->path, 716, 600, EasyThumbnailImage::THUMBNAIL_OUTBOUND, 100);
             $smallImages[] = EasyThumbnailImage::thumbnailFileUrl(Yii::getAlias('@frontend_web').$image->path, 150, 150, EasyThumbnailImage::THUMBNAIL_OUTBOUND, 100);
+            $portraitImages[] = EasyThumbnailImage::thumbnailFileUrl(Yii::getAlias('@frontend_web').$image->path, 400, 730, EasyThumbnailImage::THUMBNAIL_OUTBOUND, 100);
         }
 
         $tags = [];
@@ -47,6 +49,8 @@ class PlaceApiItem implements ApiItem
             'small_images' => $smallImages,
             'big_image' => count($bigImages) ? $bigImages[0] : '',
             'big_images' => $bigImages,
+            'portrait_image' => count($portraitImages) ? $portraitImages[0] : '',
+            'portrait_images' => $portraitImages,
             'tags' => $tags,
             'coords' => $model->coords,
             'user_status' => $userPlace ? $userPlace->status : 0,
