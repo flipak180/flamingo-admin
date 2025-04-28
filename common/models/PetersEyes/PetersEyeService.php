82f908ec90ee0a820defb70f110e134bcb5a3c61
skavorodka <flipak180@mail.ru>
2025-04-28 19:01:18 +0300

    +++

--- common/models/PetersEyes/PetersEyeService.php
+++ common/models/PetersEyes/PetersEyeService.php
@@ -25,11 +25,8 @@ class PetersEyeService
      */
     public function participate()
     {
-        $existingModel = PetersEyeUser::find()->where([
-            'peters_eye_id' => $this->model->id,
-            'user_id' => Yii::$app->user->id,
-        ])->one();
-        if ($existingModel) {
+        $currentUser = $this->getCurrentUser();
+        if ($currentUser) {
             return null;
         }
 
@@ -46,14 +43,21 @@ class PetersEyeService
      */
     public function submit($coordinates)
     {
+        $currentUser = $this->getCurrentUser();
+
         $distance = Helper::getDistance($coordinates, $this->model->coords);
-        if ($distance > $this->model->radius) {
+        if ($distance > $this->model->radius || $this->model->winner_id) {
             return false;
         }
 
         $this->model->winner_id = Yii::$app->user->id;
         $this->model->win_at = date('Y-m-d H:i:s');
-        return $this->model->save();
+        $this->model->save();
+
+        $currentUser->is_winner = true;
+        $currentUser->save();
+
+        return true;
     }
 
     /**
@@ -61,26 +65,37 @@ class PetersEyeService
      */
     public function getUserStatus()
     {
-        if (!$this->model) {
+        /** @var PetersEyeUser $petersEyeUser */
+        $petersEyeUser = PetersEyeUser::find()->where([
+            'peters_eye_id' => $this->model->id,
+            'user_id' => Yii::$app->user->id
+        ])->one();
+
+        if (!$petersEyeUser) {
             return self::STATUS_NOT_PARTICIPATE;
         }
-
-        $isParticipate = PetersEyeUser::find()
-            ->where([
-                'peters_eye_id' => $this->model->id,
-                'user_id' => Yii::$app->user->id
-            ])
-            ->exists();
-        if (!$isParticipate) {
-            return self::STATUS_NOT_PARTICIPATE;
+        if (!$this->model->winner_id) {
+            return self::STATUS_PARTICIPATE;
         }
-
-        if ($this->model->winner_id && $this->model->winner_id == Yii::$app->user->id) {
+        if ($this->model->winner_id && $petersEyeUser->is_winner) {
             return self::STATUS_WINNER;
         }
-        if ($this->model->winner_id && $this->model->winner_id != Yii::$app->user->id) {
+        if ($this->model->winner_id && !$petersEyeUser->is_winner) {
             return self::STATUS_NOT_WINNER;
         }
+    }
+
+    /**
+     * @return PetersEyeUser
+     */
+    private function getCurrentUser()
+    {
+        /** @var PetersEyeUser $model */
+        $model = PetersEyeUser::find()->where([
+            'peters_eye_id' => $this->model->id,
+            'user_id' => Yii::$app->user->id,
+        ])->one();
+        return $model;
     }
 
 }
