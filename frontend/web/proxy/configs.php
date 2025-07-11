f26d6e0f37af77b7dc84465d93348dec2b219509
skavorodka <flipak180@mail.ru>
2025-07-11 12:25:07 +0300

    +++

--- frontend/web/proxy/configs.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getConfig() {
-	return [
-		'apiKey' => 'eac8ef7807022628bb9fd897b3e69d90', // api_key из раздела Интеграции
-		'apiUrl' => 'https://dev-standalone.rnova.org:3010/api/public/', // например https://app.rnova.org/api/public/
-	];
-}
